5858bf30233210bcaf4b7007e806cb3878060664
tema <tema@mail.ru>
2025-03-29 01:20:04 +0300

    add debug function

--- rscurl.cpp
+++ rscurl.cpp
@@ -38,6 +38,12 @@ typedef struct {
     size_t size;
 } mem_t;
 
+typedef struct {
+    char trace_ascii; // 1 or 0 
+    FILE * stream;
+} debug_log_options_t;
+
+
 // CURLcode curl_easy_setopt(CURL *handle, CURLOPT_WRITEFUNCTION, write_callback);
 // CURLcode curl_easy_perform(CURL *easy_handle);
 
@@ -59,7 +65,7 @@ size_t write_data(void *ptr, size_t size, size_t nmemb, FILE *stream) {
 }
 
 size_t read_data(char *ptr, size_t size, size_t nmemb, FILE *stream) {
-  return fread(ptr, size, nmemb, stream);
+    return fread(ptr, size, nmemb, stream);
 }
 
 
@@ -81,6 +87,87 @@ size_t grow_buffer(void *contents, size_t size, size_t nmemb, void *ctx) {
     //print("%s\n", mem->buf);
 
     return realsize;
+}
+
+static void debug_dump(const char *text, FILE *stream, unsigned char *ptr, size_t size, char nohex) {
+    size_t i;
+    size_t c;
+ 
+    unsigned int width = 0x10;
+ 
+    if (nohex) // without the hex output, we can fit more on screen
+        width = 0x40;
+ 
+    fprintf(stream, "%s, %10.10lu bytes (0x%8.8lx)\n", text, (unsigned long)size, (unsigned long)size);
+ 
+    for (i = 0; i < size; i += width) {
+ 
+        fprintf(stream, "%4.4lx: ", (unsigned long)i);
+ 
+        if (!nohex) { // hex not disabled, show it 
+            for(c = 0; c < width; c++)
+                if (i + c < size)
+                    fprintf(stream, "%02x ", ptr[i + c]);
+                else
+                    fputs("   ", stream);
+        }
+ 
+        for (c = 0; (c < width) && (i + c < size); c++) {
+            // check for 0D0A; if found, skip past and start a new line of output
+            if (nohex && (i + c + 1 < size) && ptr[i + c] == 0x0D &&  ptr[i + c + 1] == 0x0A) {
+                i += (c + 2 - width);
+                break;
+            }
+
+            fprintf(stream, "%c", (ptr[i + c] >= 0x20) && (ptr[i + c] < 0x80) ? ptr[i + c] : '.');
+
+            // check again for 0D0A, to avoid an extra \n if it's at width 
+            if (nohex && (i + c + 2 < size) && ptr[i + c + 1] == 0x0D && ptr[i + c + 2] == 0x0A) {
+                i += (c + 3 - width);
+                break;
+            }
+        }
+        fputc('\n', stream); // newline
+    }
+    fflush(stream);
+}
+
+static int debug_log(CURL *handle, curl_infotype type, char *data, size_t size, void *user_p) {
+    debug_log_options_t * config = (debug_log_options_t *)user_p;
+    const char *text;
+
+    if (config->stream == NULL)
+        return 1;
+
+    switch(type) {
+    case CURLINFO_TEXT:
+        fprintf(config->stream, "== Info: %s", data);
+        return 0;
+    case CURLINFO_HEADER_OUT:
+        text = "=> Send header";
+        break;
+    case CURLINFO_DATA_OUT:
+        text = "=> Send data";
+        break;
+    case CURLINFO_SSL_DATA_OUT:
+        text = "=> Send SSL data";
+        break;
+    case CURLINFO_HEADER_IN:
+        text = "<= Recv header";
+        break;
+    case CURLINFO_DATA_IN:
+        text = "<= Recv data";
+        break;
+    case CURLINFO_SSL_DATA_IN:
+        text = "<= Recv SSL data";
+        break;
+    default: /* in case a new one is introduced to shock us */
+        return 0;
+    }
+
+    debug_dump(text, config->stream, (unsigned char *)data, size, config->trace_ascii);
+
+    return 0;
 }
 
 char * rsGetStringParam(int iParam, char * defStr) {
@@ -159,6 +246,9 @@ public:
         CURLcode curl_ret;
 
         func_curl_slist_free_all(headers);
+
+        if (debug_log_options.stream) 
+            fclose(debug_log_options.stream);
 
         if (curl) 
             curl_ret = func_curl_easy_cleanup(curl);
@@ -326,7 +416,6 @@ public:
     }
 
     RSL_METHOD_DECL(EasySetWriteString) {
-        VALUE * vString;
         CURLcode curl_ret;
 
         if (mem_in != NULL) {
@@ -340,6 +429,7 @@ public:
             free(mem_in);
             mem_in = NULL;
         }
+
         mem_in = (mem_t *)malloc(sizeof(mem_t));
         mem_in->size = 0;
         mem_in->buf = (char*)malloc(1);
@@ -465,13 +555,45 @@ public:
         return 0;
     }
 
-
     RSL_METHOD_DECL(ClearHeaders) {
         func_curl_slist_free_all(headers);
         headers = NULL;
         return 0;
     }
 
+    RSL_METHOD_DECL(SetDebugLog) {
+        VALUE * vString;
+        int ret = 0;
+
+        debug_log_options.trace_ascii = 1;
+
+        if (!GetParm(1, &vString) || vString->v_type != V_STRING) {
+            ReturnVal(V_BOOL, &ret);
+            return 0;
+        }
+
+        if ((debug_log_options.stream = fopen(vString->value.string, "wt")) == NULL) {
+            ReturnVal(V_BOOL, &ret);
+            return 0;
+        }
+
+        if (func_curl_easy_setopt(curl, CURLOPT_DEBUGFUNCTION, debug_log)) {
+            ReturnVal(V_BOOL, &ret);
+            return 0;
+        };
+
+        if (func_curl_easy_setopt(curl, CURLOPT_DEBUGDATA, &debug_log_options)) {
+            ReturnVal(V_BOOL, &ret);
+            return 0;
+        };
+
+        func_curl_easy_setopt(curl, CURLOPT_VERBOSE, (void *)1);
+
+        ret = 1;
+        ReturnVal(V_BOOL, &ret);
+        return 0;
+
+    }
 
     RSL_METHOD_DECL(EasyPerform) {
         CURLcode curl_ret;
@@ -558,6 +680,8 @@ private:
     FILE * f_out = NULL;
     mem_t * mem_in = NULL;
     struct curl_slist *headers = NULL;
+    debug_log_options_t debug_log_options = {1, NULL};
+
 };
 
 //TRslParmsInfo prmOneStr[] = { {V_STRING,0} };
@@ -584,12 +708,11 @@ RSL_CLASS_BEGIN(CRScUrl)
     RSL_METH       (ClearString)
     RSL_METH       (AddHeader)
     RSL_METH       (ClearHeaders)
+    RSL_METH       (SetDebugLog)
 
     RSL_INIT
 
 RSL_CLASS_END  
-
-
 
 EXP32 void DLMAPI EXP AddModuleObjects (void) {
     RslAddUniClass (CRScUrl::TablePtr,true);
